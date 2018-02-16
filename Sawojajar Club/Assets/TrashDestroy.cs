7c5a8d9ac852872778cb6c8f4ff64b3c71a1a114
Rizal Kholif <rizalhellboy@gmail.com>
2018-02-16 07:00:58 +0700

    First Commit
    Game Trash

---
+++ Sawojajar Club/Assets/TrashDestroy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TrashDestroy : MonoBehaviour {
+	public GameObject enemy;
+
+	void Start(){
+	
+	}
+
+	void Update(){
+	
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		switch(other.gameObject.tag)
+		{
+		case "TongSampah":
+			Destroy (this.gameObject);
+			Debug.Log ("Masuk");
+			break;
+		case "Plan":
+			Destroy (this.gameObject);
+			Debug.Log ("Tidak Masuk");
+			break;
+		}
+
+	}
+
+
+}
